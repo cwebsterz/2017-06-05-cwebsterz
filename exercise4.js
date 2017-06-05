46d0c4e213349ed1c91eed822805e28a2661454a
cwebsterz <cwebsterzornes@live.com>
2017-06-05 10:20:59 -0400

    added ramda and finished 4th exercise

---
+++ exercise4.js
@@ -0,0 +1,11 @@
+var slappaDaArrayMon = {
+    firstName: Bob,
+    lastName: Marley,
+    age: 45,
+    middleName: Nesta,
+    favoriteMusic: Reggae,
+    isRasta: true
+  }
+
+
+console.log(R.invert(slappaDaArrayMon))
